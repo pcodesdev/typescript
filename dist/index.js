1528928cc57ea4dc96683a570a5a64bc998428a4
Peter Njuguna <njugunapm23@gmail.com>
2023-03-05 12:52:14 +0300

    typescript built-in data type

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,24 @@
 let age = 20;
 if (age < 10)
     age += 10;
-//# sourceMappingURL=index.js.map
+let sales = 1200100;
+let score;
+let peopleAge = [56, 78, 23];
+let ageName = [1, "Joseph"];
+let textColor = 2;
+console.log(textColor);
+const calculateTax = (income, taxYear = 2022) => {
+    if (taxYear === 2022 && income >= 24000)
+        return income * 0.16;
+    return income;
+};
+console.log(calculateTax(100000, 2023));
+const employee = {
+    id: 1,
+    age: 27,
+    name: "Peter",
+    retires: (date) => {
+        console.log(date);
+    },
+};
+employee.retires(2016);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
 let age: number = 20;
 if (age < 10) age += 10;
+
+// Built-in type
+let sales: number = 1_200_100;
+// Any type
+let score: any;
+
+// Array
+let peopleAge: number[] = [56, 78, 23];
+
+// Tuples
+let ageName: [number, string] = [1, "Joseph"];
+
+// Enum
+const enum color {
+  red,
+  green,
+  blue,
+}
+let textColor: color = color.blue;
+console.log(textColor);
+
+// Function
+
+const calculateTax = (income: number, taxYear: number = 2022): number => {
+  if (taxYear === 2022 && income >= 24000) return income * 0.16;
+  return income;
+};
+console.log(calculateTax(100000, 2023));
+
+// Objects
+const employee: {
+  readonly id: number;
+  age: number;
+  name: string;
+  retires: (date: number) => void;
+} = {
+  id: 1,
+  age: 27,
+  name: "Peter",
+  retires: (date) => {
+    console.log(date);
+  },
+};
+employee.retires(2016);
