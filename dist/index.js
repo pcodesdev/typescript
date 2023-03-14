71354a47fc7ea14714916a7ebfacfa2ce7938ca8
Peter Njuguna <njugunapm23@gmail.com>
2023-03-14 06:09:56 +0300

    typescript advanced types

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let age = 20;
 if (age < 10)
     age += 10;
@@ -23,3 +24,44 @@ const employee = {
     },
 };
 employee.retires(2016);
+const kgToLbs = (weight) => {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+};
+kgToLbs(20);
+kgToLbs("40");
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+function personName(name) {
+    if (name)
+        console.log(name.toLocaleUpperCase());
+    else
+        console.log("Sasa");
+}
+personName(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+let phone = document.getElementById("phone");
+phone.value;
+function render(doc) {
+    if (typeof doc === "string")
+        console.log(doc);
+}
+function processEvents() {
+    while (true) {
+    }
+}
+processEvents();
+console.log("I love coding in typeScript");
--- src/index.ts
+++ src/index.ts
@@ -29,13 +29,16 @@ const calculateTax = (income: number, taxYear: number = 2022): number => {
 };
 console.log(calculateTax(100000, 2023));
 
-// Objects
-const employee: {
+// Type Aliase
+type Employee = {
   readonly id: number;
   age: number;
   name: string;
   retires: (date: number) => void;
-} = {
+};
+
+// Objects
+const employee: Employee = {
   id: 1,
   age: 27,
   name: "Peter",
@@ -44,3 +47,80 @@ const employee: {
   },
 };
 employee.retires(2016);
+
+// Union Types
+const kgToLbs = (weight: number | string): number => {
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+};
+
+kgToLbs(20);
+kgToLbs("40");
+
+// Intersection Type
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// Literal (exact,specific)
+type Quantity = 20 | 50;
+let quantity: Quantity = 50;
+
+// Nullable Types
+function personName(name: string | null) {
+  if (name) console.log(name.toLocaleUpperCase());
+  else console.log("Sasa");
+}
+personName(null);
+
+// Optional Chaining
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// The Nullish Coaelscing operator
+let speed: number | null = null;
+let ride = {
+  // Falsy values in js (undefined,null,"",false,0)
+  speed: speed ?? 30,
+};
+
+// Type Assertions
+let phone = document.getElementById("phone") as HTMLInputElement;
+
+phone.value;
+
+// The Unknown Type
+// Remember using the unknown type is preferred to using any type
+function render(doc: unknown) {
+  // Narrowing
+  if (typeof doc === "string") console.log(doc);
+}
+
+//The Never Type
+function processEvents(): never {
+  while (true) {
+    // Infinite loop
+  }
+}
+processEvents();
+console.log("I love coding in typeScript");
