40b59ce0079155cc4155cbf3b91445f4878a371c
Peter Njuguna <njugunapm23@gmail.com>
2023-03-18 12:06:29 +0300

    introduction to classes in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,67 +1,26 @@
 "use strict";
-var _a;
-let age = 20;
-if (age < 10)
-    age += 10;
-let sales = 1200100;
-let score;
-let peopleAge = [56, 78, 23];
-let ageName = [1, "Joseph"];
-let textColor = 2;
-console.log(textColor);
-const calculateTax = (income, taxYear = 2022) => {
-    if (taxYear === 2022 && income >= 24000)
-        return income * 0.16;
-    return income;
-};
-console.log(calculateTax(100000, 2023));
-const employee = {
-    id: 1,
-    age: 27,
-    name: "Peter",
-    retires: (date) => {
-        console.log(date);
-    },
-};
-employee.retires(2016);
-const kgToLbs = (weight) => {
-    if (typeof weight === "number")
-        return weight * 2.2;
-    else
-        return parseInt(weight) * 2.2;
-};
-kgToLbs(20);
-kgToLbs("40");
-let textBox = {
-    drag: () => { },
-    resize: () => { },
-};
-let quantity = 50;
-function personName(name) {
-    if (name)
-        console.log(name.toLocaleUpperCase());
-    else
-        console.log("Sasa");
-}
-personName(null);
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(0);
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
-let speed = null;
-let ride = {
-    speed: speed !== null && speed !== void 0 ? speed : 30,
-};
-let phone = document.getElementById("phone");
-phone.value;
-function render(doc) {
-    if (typeof doc === "string")
-        console.log(doc);
-}
-function processEvents() {
-    while (true) {
+class Account {
+    constructor(id, owner, _balance, username) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = _balance;
+        this.username = username;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this._balance += amount;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(val) {
+        if (val < 0)
+            throw new Error("Invalid value");
+        this._balance = val;
     }
 }
-processEvents();
-console.log("I love coding in typeScript");
+const account = new Account(1, "Peter", 70);
+account.deposit(100);
+account.balance = 100;
+console.log(account.balance);
--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,184 @@
-let age: number = 20;
-if (age < 10) age += 10;
+// let age: number = 20;
+// if (age < 10) age += 10;
 
-// Built-in type
-let sales: number = 1_200_100;
-// Any type
-let score: any;
+// // Built-in type
+// let sales: number = 1_200_100;
+// // Any type
+// let score: any;
 
-// Array
-let peopleAge: number[] = [56, 78, 23];
+// // Array
+// let peopleAge: number[] = [56, 78, 23];
 
-// Tuples
-let ageName: [number, string] = [1, "Joseph"];
+// // Tuples
+// let ageName: [number, string] = [1, "Joseph"];
 
-// Enum
-const enum color {
-  red,
-  green,
-  blue,
+// // Enum
+// const enum color {
+//   red,
+//   green,
+//   blue,
+// }
+// let textColor: color = color.blue;
+// console.log(textColor);
+
+// // Function
+
+// const calculateTax = (income: number, taxYear: number = 2022): number => {
+//   if (taxYear === 2022 && income >= 24000) return income * 0.16;
+//   return income;
+// };
+// console.log(calculateTax(100000, 2023));
+
+// // Type Aliase
+// type Employee = {
+//   readonly id: number;
+//   age: number;
+//   name: string;
+//   retires: (date: number) => void;
+// };
+
+// // Objects
+// const employee: Employee = {
+//   id: 1,
+//   age: 27,
+//   name: "Peter",
+//   retires: (date) => {
+//     console.log(date);
+//   },
+// };
+// employee.retires(2016);
+
+// // Union Types
+// const kgToLbs = (weight: number | string): number => {
+//   if (typeof weight === "number") return weight * 2.2;
+//   else return parseInt(weight) * 2.2;
+// };
+
+// kgToLbs(20);
+// kgToLbs("40");
+
+// // Intersection Type
+// type Draggable = {
+//   drag: () => void;
+// };
+
+// type Resizable = {
+//   resize: () => void;
+// };
+
+// type UIWidget = Draggable & Resizable;
+
+// let textBox: UIWidget = {
+//   drag: () => {},
+//   resize: () => {},
+// };
+
+// // Literal (exact,specific)
+// type Quantity = 20 | 50;
+// let quantity: Quantity = 50;
+
+// // Nullable Types
+// function personName(name: string | null) {
+//   if (name) console.log(name.toLocaleUpperCase());
+//   else console.log("Sasa");
+// }
+// personName(null);
+
+// // Optional Chaining
+// type Customer = {
+//   birthday: Date;
+// };
+
+// function getCustomer(id: number): Customer | null | undefined {
+//   return id === 0 ? null : { birthday: new Date() };
+// }
+
+// let customer = getCustomer(0);
+// // Optional property access operator
+// console.log(customer?.birthday?.getFullYear());
+
+// // The Nullish Coaelscing operator
+// let speed: number | null = null;
+// let ride = {
+//   // Falsy values in js (undefined,null,"",false,0)
+//   speed: speed ?? 30,
+// };
+
+// // Type Assertions
+// let phone = document.getElementById("phone") as HTMLInputElement;
+
+// phone.value;
+
+// // The Unknown Type
+// // Remember using the unknown type is preferred to using any type
+// function render(doc: unknown) {
+//   // Narrowing
+//   if (typeof doc === "string") console.log(doc);
+// }
+
+// //The Never Type
+// function processEvents(): never {
+//   while (true) {
+//     // Infinite loop
+//   }
+// }
+// processEvents();
+// console.log("I love coding in typeScript");
+
+// // Creating Classes
+class Account {
+  // Parameter properties
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number,
+
+    public username?: string
+  ) {}
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this._balance += amount;
+  }
+  // getter
+  get balance(): number {
+    return this._balance;
+  }
+  // setter
+  set balance(val: number) {
+    if (val < 0) throw new Error("Invalid value");
+    this._balance = val;
+  }
+  // private calcTax(): void {}
 }
-let textColor: color = color.blue;
-console.log(textColor);
+// Creating Objects
+const account = new Account(1, "Peter", 70);
+account.deposit(100);
+account.balance = 100;
+console.log(account.balance);
 
-// Function
-
-const calculateTax = (income: number, taxYear: number = 2022): number => {
-  if (taxYear === 2022 && income >= 24000) return income * 0.16;
-  return income;
-};
-console.log(calculateTax(100000, 2023));
-
-// Type Aliase
-type Employee = {
-  readonly id: number;
-  age: number;
-  name: string;
-  retires: (date: number) => void;
-};
-
-// Objects
-const employee: Employee = {
-  id: 1,
-  age: 27,
-  name: "Peter",
-  retires: (date) => {
-    console.log(date);
-  },
-};
-employee.retires(2016);
-
-// Union Types
-const kgToLbs = (weight: number | string): number => {
-  if (typeof weight === "number") return weight * 2.2;
-  else return parseInt(weight) * 2.2;
-};
-
-kgToLbs(20);
-kgToLbs("40");
-
-// Intersection Type
-type Draggable = {
-  drag: () => void;
-};
-
-type Resizable = {
-  resize: () => void;
-};
-
-type UIWidget = Draggable & Resizable;
-
-let textBox: UIWidget = {
-  drag: () => {},
-  resize: () => {},
-};
-
-// Literal (exact,specific)
-type Quantity = 20 | 50;
-let quantity: Quantity = 50;
-
-// Nullable Types
-function personName(name: string | null) {
-  if (name) console.log(name.toLocaleUpperCase());
-  else console.log("Sasa");
-}
-personName(null);
-
-// Optional Chaining
-type Customer = {
-  birthday: Date;
-};
-
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { birthday: new Date() };
+class SeatArrangement {
+  // Index signature property
+  [seatNumber: string]: string;
 }
 
-let customer = getCustomer(0);
-// Optional property access operator
-console.log(customer?.birthday?.getFullYear());
+let seats = new SeatArrangement();
+seats.a1 = "Peter";
 
-// The Nullish Coaelscing operator
-let speed: number | null = null;
-let ride = {
-  // Falsy values in js (undefined,null,"",false,0)
-  speed: speed ?? 30,
-};
-
-// Type Assertions
-let phone = document.getElementById("phone") as HTMLInputElement;
-
-phone.value;
-
-// The Unknown Type
-// Remember using the unknown type is preferred to using any type
-function render(doc: unknown) {
-  // Narrowing
-  if (typeof doc === "string") console.log(doc);
-}
-
-//The Never Type
-function processEvents(): never {
-  while (true) {
-    // Infinite loop
+// Static members
+class Ride {
+  private static _activeRides: number = 0;
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+  static get activeRides() {
+    return Ride._activeRides;
   }
 }
-processEvents();
-console.log("I love coding in typeScript");
+
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
